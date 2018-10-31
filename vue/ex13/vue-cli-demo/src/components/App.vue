25610378892f56698d5abc82d12d9b95e70ebc4d
onloadcc <zchongzhi@qq.com>
2018-10-31 19:51:53 +0700

    vue-router ex add

---
+++ vue/ex13/vue-cli-demo/src/components/App.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <div>
+        <h1>demo components</h1>
+
+        <ul>
+            <li>
+                <router-link to="/">Hello world</router-link>
+            </li>
+            <li>
+                <router-link to="markdown">Markdown</router-link>
+            </li>
+            <li>
+                <router-link to="github">Github</router-link>
+            </li>
+            <li>
+                <router-link to="gridView">GridView</router-link>
+            </li>
+            <li>
+                <router-link to="treeView">TreeView</router-link>
+            </li>
+            <li>
+                <router-link to="chart">Chart</router-link>
+            </li>
+            <li>
+                <router-link to="modal">Modal</router-link>
+            </li>
+        </ul>
+
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data: function () {
+            return {
+                showModal: false
+            }
+        }
+    }
+</script>
--- vue/ex13/vue-cli-demo/src/main.js
+++ vue/ex13/vue-cli-demo/src/main.js
@@ -1,11 +1,56 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from './components/App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 
 
 Vue.use(VueResource)
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+import HelloWorld from './components/HelloWorld.vue'
+import Markdown from './components/markdown.vue'
+import Github from './components/github.vue'
+import GridView from './components/GridView.vue'
+import TreeViewDemo from './components/TreeViewDemo.vue'
+import Chart from './components/chart.vue'
+import modal from './components/Modal.vue'
+
+
+const ModalView = {
+    // component: {
+    //     modal: Modal,
+    // },
+    template: `
+     <button id="show-modal" @click="showModal = true">Show Modal</button>
+         <!-- use the modal component, pass in the prop -->
+         <modal v-if="showModal" @close="showModal = false">
+             <!--
+               you can use custom content here to overwrite
+               default content
+             -->
+             <h3 slot="header">custom header</h3>
+         </modal>
+    `,
+}
+const router = new VueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        // {path: '', component: HelloWorld},
+        {path: '/markdown', component: Markdown},
+        {path: '/github', component: Github},
+        {path: '/gridView', component: GridView},
+        {path: '/treeView', component: TreeViewDemo},
+        {path: '/chart', component: Chart},
+        {path: '/modal', component: ModalView},
+        {path: '*', component: HelloWorld}
+    ]
+})
+
+
 new Vue({
+    router,
     render: h => h(App)
 }).$mount('#app')
